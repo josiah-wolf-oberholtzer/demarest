f41b2c58066e1967764c5258323cccf4e5546fc9
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-04-17 22:17:47 -0700

    Implementing ScoreTemplate and SegmentMaker.

--- demarest/tools/ScoreTemplate.py
+++ demarest/tools/ScoreTemplate.py
@@ -1,23 +1,121 @@
 # -*- coding: utf-8 -*-
-from abjad import *
+import consort
+from abjad.tools import instrumenttools
+from abjad.tools import scoretools
 
 
-class ScoreTemplate(object):
-    r'''
-    '''
+class ScoreTemplate(consort.ScoreTemplate):
 
     ### CLASS VARIABLES ###
 
     __slots__ = ()
 
-    ### INITIALIZER ###
-
-    def __init__(self):
-        pass
-
     ### SPECIAL METHODS ###
 
     def __call__(self):
-        pass
 
-    ### PUBLIC PROPERTIES ###
+        time_signature_context = scoretools.Context(
+            context_name='TimeSignatureContext',
+            name='Time Signature Context',
+            )
+        self._attach_tag('time', time_signature_context)
+
+        performer_1 = self._make_staff(
+            'Performer 1', 'percussion',
+            abbreviation='p_1',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-1',
+            )
+
+        performer_2 = self._make_staff(
+            'Performer 2', 'percussion',
+            abbreviation='p_2',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-2',
+            )
+
+        performer_3 = self._make_staff(
+            'Performer 3', 'percussion',
+            abbreviation='p_3',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-3',
+            )
+
+        performer_4 = self._make_staff(
+            'Performer 4', 'percussion',
+            abbreviation='p_4',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-4',
+            )
+
+        performer_5 = self._make_staff(
+            'Performer 5', 'percussion',
+            abbreviation='p_5',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-5',
+            )
+
+        performer_6 = self._make_staff(
+            'Performer 6', 'percussion',
+            abbreviation='p_6',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-6',
+            )
+
+        performer_7 = self._make_staff(
+            'Performer 7', 'percussion',
+            abbreviation='p_7',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-7',
+            )
+
+        performer_8 = self._make_staff(
+            'Performer 8', 'percussion',
+            abbreviation='p_8',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-8',
+            )
+
+        performer_9 = self._make_staff(
+            'Performer 9', 'percussion',
+            abbreviation='p_9',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-9',
+            )
+
+        performer_10 = self._make_staff(
+            'Performer 10', 'percussion',
+            abbreviation='p_10',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-10',
+            )
+
+        performer_11 = self._make_staff(
+            'Performer 11', 'percussion',
+            abbreviation='p_11',
+            instrument=instrumenttools.Percussion(),
+            tag='performer-11',
+            )
+
+        staff_group = scoretools.StaffGroup(
+            [
+                performer_1,
+                performer_2,
+                performer_3,
+                performer_4,
+                performer_5,
+                performer_6,
+                performer_7,
+                performer_8,
+                performer_9,
+                performer_10,
+                performer_11,
+                ],
+            name='Staff Group',
+            )
+
+        score = scoretools.Score(
+            [time_signature_context, staff_group],
+            name='Demarest Score',
+            )
+        return score
--- demarest/tools/SegmentMaker.py
+++ demarest/tools/SegmentMaker.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-from abjad import *
+# -*- encoding: utf-8 -*-
+from abjad.tools import durationtools
+from abjad.tools import markuptools
+import consort
 
 
-class SegmentMaker(object):
-    r'''
-    '''
+class SegmentMaker(consort.SegmentMaker):
 
     ### CLASS VARIABLES ###
 
@@ -12,12 +12,58 @@ class SegmentMaker(object):
 
     ### INITIALIZER ###
 
-    def __init__(self):
-        pass
-
-    ### SPECIAL METHODS ###
-
-    def __call__(self):
-        pass
+    def __init__(
+        self,
+        annotate_colors=None,
+        annotate_phrasing=None,
+        annotate_timespans=None,
+        desired_duration_in_seconds=None,
+        discard_final_silence=None,
+        maximum_meter_run_length=None,
+        name=None,
+        permitted_time_signatures=None,
+        repeat=None,
+        score_template=None,
+        settings=None,
+        tempo=None,
+        ):
+        import demarest
+        score_template = score_template or demarest.ScoreTemplate()
+        consort.SegmentMaker.__init__(
+            self,
+            annotate_colors=annotate_colors,
+            annotate_phrasing=annotate_phrasing,
+            annotate_timespans=annotate_timespans,
+            desired_duration_in_seconds=desired_duration_in_seconds,
+            discard_final_silence=discard_final_silence,
+            maximum_meter_run_length=maximum_meter_run_length,
+            name=name,
+            permitted_time_signatures=permitted_time_signatures,
+            repeat=repeat,
+            score_template=score_template,
+            settings=settings,
+            tempo=tempo,
+            timespan_quantization=durationtools.Duration(1, 8),
+            )
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def final_markup(self):
+        portland = markuptools.Markup('Portland, OR')
+        date = markuptools.Markup('January 2016 - June 2016')
+        null = markuptools.Markup.null()
+        contents = [
+            null,
+            null,
+            null,
+            portland,
+            date,
+            ]
+        markup = markuptools.Markup.right_column(contents)
+        markup = markup.italic()
+        return markup
+
+    @property
+    def score_package_name(self):
+        return 'demarest'
