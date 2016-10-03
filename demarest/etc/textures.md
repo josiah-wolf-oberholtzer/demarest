11759b2a52c0ad3f5214f74a10337f46df41b45e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-10-02 22:29:05 -0700

    Sketching out new textures.

---
+++ demarest/etc/textures.md
@@ -0,0 +1,46 @@
+| | glass | gulls | sand | stone | waves | wind | wood |
+
+| pitch pipe drone |
+| pitch pipe flutter |
+| whispered inhales |
+| whispered pointillism |
+| whispered repetitions |
+
+| guiro tap pointillism |
+| guiro tap repetitions |
+| guiro pointillism |
+| guiro repetitions |
+| guiro shimmer |
+| shaker drone |
+| shaker pointillism |
+| shaker repetitions |
+| shaker shimmer |
+| wine glass drone |
+| wine glass tap point |
+| wine glass tap rep |
+
+| crotales tranquilo |
+| marimba agitato |
+| marimba drone |
+| marimba repetitions |
+| marimba shimmer |
+| woodblock fanfare |
+
+| ratchet drone |
+| snare drone |
+| tam tam drone |
+| tam tam repetitions |
+| tam tam tranquilo |
+| vibraphone agitato |
+| vibraphone drone |
+| vibraphone repetitions |
+| vibraphone shimmer |
+| vibraphone tranquilo |
+
+| bass drum drone |
+| bass drum repetitions |
+| bass drum tranquilo |
+| toms fanfare |
+| toms repetitions |
+| tubular bells drone |
+| tubular bells tranquilo |
---
+++ demarest/segments/texture_wind/definition.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+import abjad
+import consort
+import demarest
+from demarest import materials
+
+
+### TEXTURE WIND ###
+
+segment_maker = demarest.SegmentMaker(
+    annotate_colors=True,
+    annotate_phrasing=False,
+    desired_duration_in_seconds=30,
+    permitted_time_signatures=[
+        (4, 4),
+        ],
+    tempo=abjad.Tempo((1, 4), 64),
+    )
+
+### TIMESPAN MAKERS ###
+
+droning_timespan_maker = abjad.new(
+    materials.sparse_timespan_maker,
+    playing_groupings=[4, 5],
+    fuse_groups=True,
+    )
+
+sparse_timespan_maker = abjad.new(
+    materials.sparse_timespan_maker,
+    playing_groupings=[1, 1, 2, 1, 2, 3],
+    fuse_groups=True,
+    padding=abjad.Duration(1, 4),
+    )
+
+### TIMESPAN IDENTIFIERS ###
+
+common_timespan_identifier = abjad.sequencetools.Sequence(
+    [1, -2, 1, -1, 3, 1, -1, 2, -2, 3]
+    )
+
+rare_timespan_identifier = abjad.sequencetools.Sequence(
+    [-5, 1, -3, 1, -4, 1, -2],
+    )
+
+### MUSIC SPECIFIERS ###
+
+### MELANGES ###
+
+### BACKGROUND MUSIC SETTINGS ###
+
+### MELANGE MUSIC SETTINGS ###
+
+### CASCADING MUSIC SETTINGS ###
+
+### TRIO MUSIC SPECIFIERS ###
+
+### TRIO PERCUSSION MUSIC SETTINGS ###
