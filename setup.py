fcafbbe4e118eba960fc99f50f3b509483d24d35
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-05-06 13:52:08 -0700

    Update installation files.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python
-import setuptools
+#! /usr/bin/env python
+from distutils.core import setup
 
 
 if __name__ == '__main__':
-    setuptools.setup(
+    setup(
         author='Josiah Wolf Oberholtzer',
         author_email='josiah.oberholtzer@gmail.com',
-        install_requires = (
-            'abjad',
-            ),
+        install_requires=['consort'],
         name='demarest',
-        packages=(
-            'demarest',
-            ),
+        packages=('demarest',),
         url='https://github.com/josiah-wolf-oberholtzer/demarest',
         version='0.1',
         zip_safe=False,
